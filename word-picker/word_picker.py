d952719c1178cee85c9167561de1de845c3d0d2d
u612782 <trombitas.ferenc@mbhbank.hu>
2024-10-08 15:07:00 +0200

    add k8s yaml

--- word-picker/word_picker.py
+++ word-picker/word_picker.py
@@ -1,7 +1,18 @@
 from random import choice
 from flask import Flask
+from os import environ
+
 app = Flask(__name__)
-choices = ["first", "second", "third"]
+options_file = environ.get("OPTIONS_FILE", "options.txt")
+#choices = ["first", "second", "third"]
+
+def load_options(file_path):
+  with openf(ile_path, "r", encoding="uft8") as f:
+  file_contents = f.read()
+    return file_contents.split()
+
+options = load_options(options_file)
+
 @app.route("/")
 def pick_word():
-  return choice(choices)
+  return choice(options)
